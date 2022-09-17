e4b0fc37b335c428aa0b018ee65a93676cba37af
Crutarchuk Roman <makarenaclub@mail.ru>
2022-09-17 23:13:59 +0300

    firs server message to client

--- server.js
+++ server.js
@@ -3,23 +3,28 @@ const { Server } = ws;
 import { v4 as uuid } from "uuid";
 import chalk from 'chalk'
 
-const client = {};
-const clients = [];
+const clients ={};
+const messages = [];
 
 const webSocketServer = new Server({ port: 8081 });
 webSocketServer.on("connection", (wsClient) => {
   const id = uuid();
-  client[id] = wsClient;
+  clients[id] = wsClient;
   console.log(chalk.bgGreen(`New client ${id}`));
   
 
   wsClient.on("close", () => {
-    delete client[id];
+    delete clients[id];
     console.log(chalk.bgRed(`Client is closed ${id}`));
     
   });
-  wsClient.on('message',(message)=>{
-    console.log('clientMessage',message);
+  wsClient.on('message',(JSONmessage)=>{
+    console.log('clientMessage',JSONmessage);
+    const {clientName, clientMessage} = JSON.parse(JSONmessage)
+    messages.push({clientName, clientMessage})
+  for(const id in clients){
+    clients[id].send(JSON.stringify([{clientName, clientMessage}]))
+  }
   })
 });
 
--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
       <input v-model="name" type="text" name="name" id="name" />
       <input v-model="message" type="text" name="message" id="message" />
       <button @click="send()">submit</button>
+      <button @click="log()">log</button>
     </div>
 
-    <div id="chat"></div>
+    <!-- <div id="chat" v-for="(messages,i) in clientsMessages" :key="i"><p>{{messages}}</p></div> -->
   </div>
 </template>
 
@@ -14,23 +15,46 @@
 export default {
   name: "",
   message: "",
-
+  messages: [],
+  computed: {
+    wsMessages: {
+      get() {
+        ws.onmessage = (message) => {
+          console.log(JSON.parse(message.data));
+        };
+      },
+    },
+    num: {
+      get() {
+        return 1 + 1;
+      },
+    },
+  },
   methods: {
+    log() {
+      console.log(this.wsMessages, "wsMessages");
+    },
     send: function () {
-      const name = this.name
-      const message = this.message
-      wsClient.send(JSON.stringify({
-        name , message
-      }))
-      this.name = ''
-      this.message = ''
+      const clientName = this.name;
+      const clientMessage = this.message;
+      ws.send(
+        JSON.stringify({
+          clientName,
+          clientMessage,
+        })
+      );
+      this.name = "";
+      this.message = "";
+    },
+    messages() {
+      console.log(ClientsMessages, "ClientsMessages");
     },
   },
 };
-const wsClient = new WebSocket("ws://127.0.0.1:8081");
+const ws = new WebSocket("ws://127.0.0.1:8081");
 
-log();
-function log() {
-  console.log(wsClient);
-}
+// function log(){
+//   console.log(this.clientsMessages,'clientsMessages');
+// }
+// log()
 </script>
