07485f9d4cdd4fa7ff630ab5493270811d56c05c
Crutarchuk Roman <makarenaclub@mail.ru>
2022-09-17 23:43:16 +0300

    ws connection from vue

--- src/App.vue
+++ src/App.vue
@@ -13,23 +13,11 @@
 
 <script>
 export default {
+  connection: null,
   name: "",
   message: "",
   messages: [],
-  computed: {
-    wsMessages: {
-      get() {
-        ws.onmessage = (message) => {
-          console.log(JSON.parse(message.data));
-        };
-      },
-    },
-    num: {
-      get() {
-        return 1 + 1;
-      },
-    },
-  },
+  computed: {},
   methods: {
     log() {
       console.log(this.wsMessages, "wsMessages");
@@ -37,21 +25,26 @@ export default {
     send: function () {
       const clientName = this.name;
       const clientMessage = this.message;
-      ws.send(
+      this.name = "";
+      this.message = "";
+      this.connection.send(
         JSON.stringify({
           clientName,
           clientMessage,
         })
       );
-      this.name = "";
-      this.message = "";
-    },
-    messages() {
-      console.log(ClientsMessages, "ClientsMessages");
     },
   },
+  created: function () {
+    console.log("Starting connection to WebSocket Server");
+    this.connection = new WebSocket("ws://127.0.0.1:8081");
+  },
 };
-const ws = new WebSocket("ws://127.0.0.1:8081");
+
+// const ws = new WebSocket("ws://127.0.0.1:8081");
+// ws.onmessage((message) => {
+//   console.log(message);
+// });
 
 // function log(){
 //   console.log(this.clientsMessages,'clientsMessages');
