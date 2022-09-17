44b0a8b8d473da9ac94a5306127f2b9f767a83eb
Crutarchuk Roman <makarenaclub@mail.ru>
2022-09-17 18:59:15 +0300

    server ws conection and chalk

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 import ws from "ws";
 const { Server } = ws;
 import { v4 as uuid } from "uuid";
+import chalk from 'chalk'
 
 const client = {};
 const clients = [];
 
-const wss = new Server({ port: 8081 });
-wss.on("connection", (ws) => {
+const webSocketServer = new Server({ port: 8081 });
+webSocketServer.on("connection", (wsClient) => {
   const id = uuid();
-  client[id] = ws;
-  console.log(`New client ${id}`);
-  ws.on("close", () => {
+  client[id] = wsClient;
+  console.log(chalk.bgGreen(`New client ${id}`));
+  
+
+  wsClient.on("close", () => {
     delete client[id];
-    console.log(`Client is closed ${id}`);
+    console.log(chalk.bgRed(`Client is closed ${id}`));
     
   });
+  wsClient.on('message',(message)=>{
+    console.log('clientMessage',message);
+  })
 });
+
+
+
+
+
 function log() {
   console.log("clients :", clients);
 }
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
-<div>
-  <form id="messageForm">
-        <input type="text" name="name" id="name" />
-        <input type="text" name="message" id="message" />
-        <input type="submit" value="send" />
-    </form>
-</div>
+  <div>
+    <div id="form">
+      <input v-model="name" type="text" name="name" id="name" />
+      <input v-model="message" type="text" name="message" id="message" />
+      <button @click="send()">submit</button>
+    </div>
+
+    <div id="chat"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  name: "",
+  message: "",
+
+  methods: {
+    send: function () {
+      const name = this.name
+      const message = this.message
+      wsClient.send(JSON.stringify({
+        name , message
+      }))
+      this.name = ''
+      this.message = ''
+    },
+  },
+};
+const wsClient = new WebSocket("ws://127.0.0.1:8081");
+
+log();
+function log() {
+  console.log(wsClient);
 }
-const ws = new WebSocket("ws://127.0.0.1:8081")
 </script>
